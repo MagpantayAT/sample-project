13fee75169670a500219591a237757d88a88649f
offemaria24 <mikeoffemaria@gmail.com>
2023-05-02 20:41:03 +0800

    asdasd
    adasdsdasdaasdasda

---
+++ Basicmath.py
@@ -0,0 +1,8 @@
+#New Branch 
+
+number = eval(input("Enter Number"))
+
+addition = number + number:
+    print("Addition ",addition)
+subtraction = number - 1000:
+    print("Subtraction", su subtraction)
